c2d2dbb1707c950698f2f10fd975a01af6358fe0
tnine9 <1511308033@qq.com>
2021-11-08 11:49:08 +0800

    Initial commit

---
+++ 21.11.05 FifthWord.py
@@ -0,0 +1,42 @@
+# 随机生成1000个整数;
+# 数字范围[20,100];
+# 输出所有不同的数字及其每个数字重复的次数,
+# 有能力的话升序排序打印所有数字{“数字”:”次数”}
+import random  # 引用random模块
+
+num = []
+numdict = {}
+for a in range(0, 1000):
+    num.insert(a, random.randint(20, 100))  # 随机出20至100之间的随机数
+for a in range(20, 101):
+    b = num.count(a)
+    numdict[str(a)] = b
+print(numdict)
+
+
+# 输出商品列表，用户输入序号，显示用户选中的商品
+# 商品列表：
+goods = [{"name": "电脑  ", "price": 1999},
+
+         {"name": "鼠标  ", "price": 10},
+
+         {"name": "显示器", "price": 120},
+
+         {"name": "内存  ", "price": 230}, ]
+# 1：页面显示 序号 + 商品名称 + 商品价格
+# 2：用户输入选择的商品序号，然后打印商品名称及商品价格
+# 3：如果用户输入的商品序号有误，则提示输入有误，并重新输入
+# 4：用户输入Q或者q，退出程序
+for a in range(len(goods)):
+    print(a + 1, ' ', goods[a]['name'], ' ', goods[a]['price'])
+while True:
+    try:
+        a = input('请输入商品序号')
+        if a == 'q' or a == 'Q':
+            print('退出系统')
+            break
+        else:
+            print(a, ' ', goods[int(a) - 1]['name'], ' ', goods[int(a) - 1]['price'])
+    except:
+        print('请输入正确的序号')
+        continue
