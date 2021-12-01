bb86dfa61f3c2c7c9cd3c104e67e971d622fca18
tnine9 <1511308033@qq.com>
2021-12-01 10:22:41 +0800

    Merge remote-tracking branch 'python/master'
    # Conflicts:
    #	21.11.05 FifthWord.py

--- 21.11.26 Thirteeth Word.py
+++ 21.11.26 Thirteeth Word.py
@@ -1,14 +1,23 @@
-class Counter():
+class Counter:
     num1 = 0
     num2 = 0
-    __operator = ''
-
-    def setoperator(self, operation):
-        if operation != '+' and operation != '-' and operation != '*' and operation != '/' and operation != '^':
-            print('您输入的运算符号不正确')
-        else:
-            self.__operator = operation
+    operator = ''
 
     def count(self):
-        if self.__operator == '+':
-            print
+        if self.operator == '+':
+            print('%s+%s=%s' % (self.num1, self.num1, self.num1 + self.num1))
+        elif self.operator == '-':
+            print('%s-%s=%s' % (self.num1, self.num1, self.num1 - self.num1))
+        elif self.operator == '*':
+            print('%s*%s=%s' % (self.num1, self.num1, self.num1 * self.num1))
+        elif self.operator == '/':
+            print('%s/%s=%s' % (self.num1, self.num1, self.num1 / self.num1))
+        else:
+            print('您输入的运算符有错')
+
+
+num = Counter
+num.num1 = int(input('请输入第一个运算数:'))
+num.operator = input('请输入运算符:')
+num.num2 = int(input('请输入第二个运算数:'))
+num().count()
